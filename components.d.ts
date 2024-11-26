75698d6e660b3843fe86e6bfc612a3948a4dac99
unknown <haoyuli40@gmail.com>
2024-11-26 22:02:34 +0800

    use ElTag

--- components.d.ts
+++ components.d.ts
@@ -22,6 +22,7 @@ declare module 'vue' {
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
+    ElTag: typeof import('element-plus/es')['ElTag']
     ElTimeline: typeof import('element-plus/es')['ElTimeline']
     ElTimelineItem: typeof import('element-plus/es')['ElTimelineItem']
     HeaderBar: typeof import('./src/components/HeaderBar.vue')['default']
